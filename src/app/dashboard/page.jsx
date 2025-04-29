fe28c9c27b575721bf5de8ae55fc5e062e838f5c
Sardlimad <genesis.david.08@gmail.com>
2025-04-28 23:13:05 -0400

    Eliminar comentarios

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,18 +1,9 @@
 "use client";
 import React from 'react';
-import { Box, Typography } from '@mui/material';
 import DashboardView from '../../components/views/DashboardView';
 
 export default function DashboardPage() {
   return (
-    // <Box sx={{ p: 3 }}>
-    //   <Typography variant="h4" component="h1" gutterBottom>
-    //     Dashboard
-    //   </Typography>
-    //   <Typography variant="body1">
-    //     Bienvenido al sistema de gestión de lista de espera.
-    //   </Typography>
-    // </Box>
     <DashboardView />
   );
 } 
